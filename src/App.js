4bca39728cc3c80f0b53ab2e7d7d066b9a85ab17
Karmen <katgucilatar@outlook.com>
2023-09-12 20:28:05 -0400

    created add task component and function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
+
+const App = () => {
+const [tasks, setTasks] = useState([])
+
+// add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+// delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+// toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+      task.id === id ? {...task, reminder: !task.reminder } : task
+      )
+    )
+  }
   return (
     <div className="container">
       <Header title='Task Tracker' />
+      <AddTask onAdd={addTask} />
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
     </div>
   );
 }
